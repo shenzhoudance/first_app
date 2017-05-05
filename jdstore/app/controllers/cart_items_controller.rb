32ca0d8f750dedb4d666ac4f02a1d5b27c29a76c
xiaowei <shenzhoudance@gmail.com>
2017-05-05 15:12:06 +0800

    cannot add item with quantity 0

--- jdstore/app/controllers/cart_items_controller.rb
+++ jdstore/app/controllers/cart_items_controller.rb
@@ -1,6 +1,8 @@
 class CartItemsController < ApplicationController
   before_action :authenticate_user!
 
+
+
   def destroy
       @cart = current_cart
       @cart_item = @cart.cart_items.find_by(product_id: params[:id])
@@ -14,7 +16,12 @@ class CartItemsController < ApplicationController
     def update
     @cart = current_cart
     @cart_item = @cart.cart_items.find_by(product_id: params[:id])
-    @cart_item.update(cart_item_params)
+    if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
+      @cart_item.update(cart_item_params)
+     flash[:notice] = "成功变更数量"
+   else
+     flash[:warning] = "数量不足以加入购物车"
+   end
 
     redirect_to carts_path
   end
