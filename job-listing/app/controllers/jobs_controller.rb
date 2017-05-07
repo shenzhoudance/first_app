be747c09fc7f2c862dc48a9fb094d5041de0c243
xiaowei <shenzhoudance@gmail.com>
2017-05-07 21:16:44 +0800

    implement part4

--- job-listing/app/controllers/jobs_controller.rb
+++ job-listing/app/controllers/jobs_controller.rb
@@ -7,8 +7,10 @@ def show
    end
 
    def index
-    @jobs = Job.all
+     @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+
   end
+
   def new
     @job = Job.new
   end
@@ -47,6 +49,6 @@ end
   private
 
   def job_params
-  params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+  params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email,:is_hidden)
   end
  end
