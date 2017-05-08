aeaa945bc4bb7088fec13831ed30f4de05d07a29
xiaowei <shenzhoudance@gmail.com>
2017-05-08 15:39:22 +0800

    implement step 5

--- job-listing/config/routes.rb
+++ job-listing/config/routes.rb
@@ -8,5 +8,14 @@ Rails.application.routes.draw do
  namespace :admin do
     resources :jobs
   end
-  
+
+  namespace :admin do
+  resources :jobs do
+    member do
+      post :publish
+      post :hide
+    end
+  end
+end
+
 end
