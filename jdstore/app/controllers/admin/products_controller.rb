fcbe4ba9883b2975923257953bce7139799ab430
xiaowei <shenzhoudance@gmail.com>
2017-05-22 18:58:30 +0800

    add add_to_cart button

--- jdstore/app/controllers/admin/products_controller.rb
+++ jdstore/app/controllers/admin/products_controller.rb
@@ -4,6 +4,13 @@ class Admin::ProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :admin_required
 
+
+
+  def index
+     @products = Product.all
+   end
+
+
 def new
     @product = Product.new
   end
