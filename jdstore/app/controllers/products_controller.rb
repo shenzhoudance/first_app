c8e8af9724e4e7d573cf6a9b1d499dfc86dc5eb7
xiaowei <shenzhoudance@gmail.com>
2017-05-22 19:01:29 +0800

    implement add_product_to_cart function

--- jdstore/app/controllers/products_controller.rb
+++ jdstore/app/controllers/products_controller.rb
@@ -12,5 +12,12 @@ class ProductsController < ApplicationController
      redirect_to :back
      flash[:notice] = "测试加入购物车"
    end
- 
+
+   def add_to_cart
+    @product = Product.find(params[:id])
+    current_cart.add_product_to_cart(@product)
+    redirect_to :back
+  end
+
+  
 end
