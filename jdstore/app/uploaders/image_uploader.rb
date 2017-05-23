d45c4e0435a2929ad83ea2e77b53b3edb2eed896
xiaowei <shenzhoudance@gmail.com>
2017-05-23 16:50:43 +0800

    change admin/product backend panel

--- jdstore/app/uploaders/image_uploader.rb
+++ jdstore/app/uploaders/image_uploader.rb
@@ -1,24 +1,26 @@
-# Include RMagick or MiniMagick support:
+class ImageUploader < CarrierWave::Uploader::Base
 
-# include CarrierWave::RMagick
+  # Include RMagick or MiniMagick support:
+
+  # include CarrierWave::RMagick
 
 
 
  include CarrierWave::MiniMagick
 
-# Choose what kind of storage to use for this uploader:
+  # Choose what kind of storage to use for this uploader:
 
-storage :file
-# storage :fog
+  storage :file
+  # storage :fog
 
 
-# Override the directory where uploaded files will be stored.
+  # Override the directory where uploaded files will be stored.
 
-# This is a sensible default for uploaders that are meant to be mounted:
+  # This is a sensible default for uploaders that are meant to be mounted:
 
-def store_dir
-  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-end
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
  process resize_to_fit: [800, 800]
 
