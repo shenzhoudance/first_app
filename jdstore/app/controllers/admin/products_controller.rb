671bd95307d2daacf14646bf99b215592bb57401
xiaowei <shenzhoudance@gmail.com>
2017-05-23 09:56:50 +0800

    only admin can access backend panel

--- jdstore/app/controllers/admin/products_controller.rb
+++ jdstore/app/controllers/admin/products_controller.rb
@@ -1,5 +1,9 @@
 class Admin::ProductsController < ApplicationController
-  def index
+  before_action :authenticate_user!
+  before_action :admin_required
+  layout "admin"
+
+ def index
    @products = Product.all
  end
 
