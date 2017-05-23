95dd1a40bd508bffc087e3ec1e25e7816f84165e
xiaowei <shenzhoudance@gmail.com>
2017-05-23 16:18:38 +0800

    add bootstrap to project

--- jdstore/app/assets/javascripts/application.js
+++ jdstore/app/assets/javascripts/application.js
@@ -13,6 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap/alert
-//= require bootstrap/dropdown
 //= require_tree .
--- jdstore/config/routes.rb
+++ jdstore/config/routes.rb
@@ -1,15 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'products#index'
-  get 'about/index'
-  namespace :admin do
-  resources :products
- end
-
- resources :products do
-     member do
-       post :add_to_cart
-     end
-   end
 end
