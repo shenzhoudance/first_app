4282777af114e87818a88dd30dedbfcc7e32ae1c
xiaowei <shenzhoudance@gmail.com>
2017-05-24 10:09:04 +0800

    empty cart

--- jdstore/app/controllers/application_controller.rb
+++ jdstore/app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+
    def admin_required
      if !current_user.admin?
        redirect_to "/", alert: "You are not admin."
      end
    end
-  end
+
 
     helper_method :current_cart
 
@@ -24,3 +25,5 @@ class ApplicationController < ActionController::Base
       session[:cart_id] = cart.id
       return cart
     end
+
+  end
